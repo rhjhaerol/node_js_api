537f49c86b125c2e4f7bceb9af42b21c006b9051
rhjhaerol <rhjhaerol@gmail.com>
2024-03-15 01:50:59 +0700

    update get, post, put, delete

--- index.js
+++ index.js
@@ -11,26 +11,66 @@ app.get("/", (req, res) => {
     const query = "SELECT * FROM item_tbl";
 
     db.query(query, (error, result) => {
-        response(200, result, "get all data", res);
+        if (error) throw error;
+        response(200, result, "Get all data", res);
     });
 });
 
-app.get("/find", (req, res) => {
-    const query = `SELECT item_name FROM item_tbl WHERE item_code = "${req.query.item_code}"`;
+app.get("/item/:item_code", (req, res) => {
+    const item_code = req.params.item_code;
+    const query = `SELECT * FROM item_tbl WHERE item_code = "${item_code}"`;
 
     db.query(query, (error, result) => {
-        response(200, result, "find item", res);
+        if (error) throw error;
+        response(200, result, "Find the data", res);
     });
 });
 
-app.post("/login", (req, res) => {
-    console.log({ requestFromOutside: req.body });
-    res.send("Berhasil Login!");
+app.post("/item", (req, res) => {
+    const { code, name, price, stock } = req.body;
+    const query = `INSERT INTO item_tbl (item_code, item_name, item_price, item_stock) VALUES ('${code}', '${name}', ${price}, ${stock})`;
+
+    db.query(query, (error, result) => {
+        if (error) response(500, "invalid", "error", res);
+        if (result?.affectedRows) {
+            const data = {
+                isSucces: result.affectedRows,
+                id: result.insertId,
+            };
+            response(200, data, "Data added successfully", res);
+        }
+    });
 });
 
-app.put("/username", (req, res) => {
-    console.log({ updateData: req.body });
-    res.send("Berhasil ganti username");
+app.put("/item", (req, res) => {
+    const { code, name, price, stock } = req.body;
+    const query = `UPDATE item_tbl SET item_name = '${name}', item_price = ${price}, item_stock = ${stock} WHERE item_code = '${code}'`;
+
+    db.query(query, (error, result) => {
+        if (error) response(500, "invalid", "error", res);
+        if (result?.affectedRows) {
+            const data = {
+                isSucces: result.affectedRows,
+                message: result.message,
+            };
+            response(200, data, "Update data successfully", res);
+        }
+    });
+});
+
+app.delete("/item", (req, res) => {
+    const { code } = req.body;
+    const query = `DELETE FROM item_tbl WHERE item_code = '${code}'`;
+
+    db.query(query, (error, result) => {
+        if (error) response(500, "invalid", "error", res);
+        if (result?.affectedRows) {
+            const data = {
+                isDelete: result.affectedRows,
+            };
+            response(200, data, "Delete data successfully", res);
+        }
+    });
 });
 
 app.listen(port, () => {
