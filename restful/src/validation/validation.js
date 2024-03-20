f409220ce73673ae4f4510fad7b5d9a837013a7a
rhjhaerol <rhjhaerol@gmail.com>
2024-03-20 22:12:49 +0700

    update validatoin

--- restful/src/validation/validation.js
+++ restful/src/validation/validation.js
@@ -1,7 +1,10 @@
 import { responseError } from "../error/response-error.js";
 
 const validate = (schema, request) => {
-    const result = schema.validate(request);
+    const result = schema.validate(request, {
+        abortEarly: false,
+        allowUnknown: false,
+    });
     if (result.error) {
         throw new responseError(400, result.error.message);
     } else {
