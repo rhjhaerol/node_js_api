99684b7eafd662a4ce103671632a28f5c1b3e5f1
rhjhaerol <rhjhaerol@gmail.com>
2024-03-20 22:13:12 +0700

    add login user test

--- restful/test/user.test.js
+++ restful/test/user.test.js
@@ -1,26 +1,119 @@
 import supertest from "supertest";
 import { web } from "../src/application/web.js";
-import { prismaClient } from "../src/application/database.js";
+import { logger } from "../src/application/logging.js";
+import { createTestUser, removeTestUser } from "./test-util.js";
 
 describe("POST /api/users", function () {
     afterEach(async () => {
-        await prismaClient.users.deleteMany({
-            where: {
-                username: "rhjhaerol",
-            },
-        });
+        await removeTestUser();
     });
 
     it("should can register new user", async () => {
         const result = await supertest(web).post("/api/users").send({
-            username: "rhjhaerol",
+            username: "test",
             password: "rahasia",
-            name: "M Rahaji Jhaerol",
+            name: "test",
         });
 
         expect(result.status).toBe(200);
-        expect(result.body.data.username).toBe("rhjhaerol");
+        expect(result.body.data.username).toBe("test");
         expect(result.body.data.password).toBeUndefined;
-        expect(result.body.data.name).toBe("M Rahaji Jhaerol");
+        expect(result.body.data.name).toBe("test");
+    });
+
+    it("should reject if request is invalid", async () => {
+        const result = await supertest(web).post("/api/users").send({
+            username: "",
+            password: "",
+            name: "",
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined;
+    });
+
+    it("should can register new user", async () => {
+        let result = await supertest(web).post("/api/users").send({
+            username: "test",
+            password: "rahasia",
+            name: "test",
+        });
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.username).toBe("test");
+        expect(result.body.data.password).toBeUndefined;
+        expect(result.body.data.name).toBe("test");
+
+        result = await supertest(web).post("/api/users").send({
+            username: "test",
+            password: "rahasia",
+            name: "test",
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined;
+    });
+});
+
+describe("POST /api/users/login", function () {
+    beforeEach(async () => {
+        await createTestUser();
+    });
+
+    afterEach(async () => {
+        await removeTestUser();
+    });
+
+    it("should can login", async () => {
+        const result = await supertest(web).post("/api/users/login").send({
+            username: "test",
+            password: "rahasia",
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(200);
+        expect(result.body.data.token).toBeDefined;
+        expect(result.body.data.token).not.toBe("test");
+    });
+
+    it("should reject login if request is invalid", async () => {
+        const result = await supertest(web).post("/api/users/login").send({
+            username: "",
+            password: "",
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(400);
+        expect(result.body.errors).toBeDefined;
+    });
+
+    it("should reject login if password is wrong", async () => {
+        const result = await supertest(web).post("/api/users/login").send({
+            username: "test",
+            password: "salah",
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined;
+    });
+
+    it("should reject login if username is wrong", async () => {
+        const result = await supertest(web).post("/api/users/login").send({
+            username: "salah",
+            password: "salah",
+        });
+
+        logger.info(result.body);
+
+        expect(result.status).toBe(401);
+        expect(result.body.errors).toBeDefined;
     });
 });
