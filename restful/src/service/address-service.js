5bebe695afc2c1c7897cc9386b8197f9978238f8
rhjhaerol <rhjhaerol@gmail.com>
2024-03-25 14:17:24 +0700

    add address update api

--- restful/src/service/address-service.js
+++ restful/src/service/address-service.js
@@ -70,7 +70,45 @@ const get = async (user, contactId, addressId) => {
     return address;
 };
 
+const update = async (user, contactId, request) => {
+    contactId = await checkContactMustExists(user, contactId);
+    const address = validate(updateAddressValidation, request);
+
+    const totalAddressInDatabase = await prismaClient.address.count({
+        where: {
+            contact_id: contactId,
+            id: address.id,
+        },
+    });
+
+    if (totalAddressInDatabase !== 1) {
+        throw new responseError(404, "address is not found");
+    }
+
+    return prismaClient.address.update({
+        where: {
+            id: address.id,
+        },
+        data: {
+            street: address.street,
+            city: address.city,
+            province: address.province,
+            country: address.country,
+            postal_code: address.postal_code,
+        },
+        select: {
+            id: true,
+            street: true,
+            city: true,
+            province: true,
+            country: true,
+            postal_code: true,
+        },
+    });
+};
+
 export default {
     create,
     get,
+    update,
 };
